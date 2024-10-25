23a139ae2c6511b551db9bc172c69c9abd0d6d25
krish kumar singh <krishkumarsingh9939@gmail.com>
2024-10-25 13:18:20 +0530

    all setup configure

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,14 +12,19 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if(!localFilePath) return null
-        const response = await  cloudinary.uploader.upload(localFilePath, {
+        const response =    await  cloudinary.uploader.upload(localFilePath, {
                 resource_type: "auto"
             })
-        console.log("file is uploaded on cloudinary");
-        
+        console.log("file is uploaded on cloudinary", response.url);
+
+        return response
+
     } catch (error) {
-        
+        fs.unlinkSync(localFilePath)
+        return null
     }
 }
 
+
+export {uploadOnCloudinary}
 
