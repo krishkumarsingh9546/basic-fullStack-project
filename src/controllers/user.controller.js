75e53681f65685bffe9cd1e444af95b23ddef4d9
krish kumar singh <krishkumarsingh9939@gmail.com>
2024-11-14 14:05:59 +0530

    add tweet like comment and palylist models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -153,7 +153,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
 
 const logoutUser = asyncHandler(async (req, res) => {
-    User.findByIdAndUpdate(
+    await User.findByIdAndUpdate(
         req.user._id,
         {
             $set: {
